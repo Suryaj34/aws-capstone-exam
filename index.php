476f292367e1d0e7c2c435cdc576631d2d4a2ffb
Suryaj34 <165369942+Suryaj34@users.noreply.github.com>
2026-02-06 06:01:19 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
-echo "Welcome to Streamline - v1 <br>";
+echo "<div style='background-color: lightblue;'>";
+echo "Welcome to StreamLine - v2 [New Feature] <br>";
 echo "IP Address: " . $_SERVER['SERVER_ADDR'];
-?>
+echo "</div>";
